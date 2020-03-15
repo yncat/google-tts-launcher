98ac24c0fe4d448e7d1c502ed0bd5de02945cfd7
nyanchan <personal@nyanchangames.com>
2020-03-15 22:13:38 +0900

    Still buggy, but up-down select control

--- ttssetting.py
+++ ttssetting.py
@@ -2,18 +2,20 @@
 #Google tts setting tool
 #Copyright (C) 2020 Yukio Nozawa <personal@nyanchangames.com>
 
+from getch import getch
 import json
+import platform
 import os
 
 DEFAULT_SETTINGS={
 	'speaking_rate': 1.0,
 	'pitch': 0,
 	'volume_gain_db': 0,
-	'effects_profile_id': None
+	'effects_profile_id': 'None'
 }
 
 PROFILES=[
-	(None, 'なし'),
+	('None', 'なし'),
 	('wearable-class-device', 'スマートウォッチやその他のウェアラブル（Apple Watch、Wear OS watch など）'),
 	('handset-class-device', 'スマートフォン（Google Pixel、Samsung Galaxy、Apple iPhone など）'),
 	('headphone-class-device', 'オーディオ再生用のイヤフォンやヘッドフォン（Sennheiser ヘッドフォンなど）'),
@@ -25,6 +27,36 @@ PROFILES=[
 ]
 
 SETTINGS_FILE_NAME='settings.json'
+
+KEY_UP=0
+KEY_DOWN=1
+KEY_RETURN=2
+IS_WINDOWS=platform.system()=='Windows'
+
+def getConsoleArrowInput():
+	return _getConsoleArrowInput_win() if IS_WINDOWS else _getConsoleArrowInput_nonWin()
+
+def _getConsoleArrowInput_win():
+	key=ord(getch())
+	if key==13: return KEY_RETURN
+	if key==224:
+		key=ord(getch())
+		if key==80: return KEY_DOWN
+		if key==72: return KEY_UP
+	#end 224
+	return -1
+#end _getConsoleArrowInput_win
+
+def _getConsoleArrowInput_nonWin():
+	key=ord(getch())
+	if key==13: return KEY_RETURN
+	if key==0x1b:
+		key=ord(getch())
+		if key==0x5b:
+			key=ord(getch())
+			if key==0x41: return KEY_UP
+			if key==0x42: return KEY_DOWN
+	return -1
 
 def checkValue(val,min,max):
 	try:
@@ -53,10 +85,16 @@ def inputWithCheck(description_text,default,min,max):
 	return ret
 
 def _printChoice(tup,first=False):
+	s=", ".join(tup)
+	if len(s)>80: s=s[0:80]
+	if len(s)<80:
+		l=[' '] * (90-len(s))
+		s+=(''.join(l))
+	#end padding
 	if first:
-		print("%s\n%s" % tup)
+		print("%s" % (s), end="",flush=True)
 	else:
-		print("\r\r%s\n%s" % tup)
+		print("\r%s" % (s), end="",flush=True)
 	#end print
 #end _printChoice
 
@@ -71,15 +109,15 @@ def showMenu(description_text,selection_tuple,default):
 		i+=1
 	#end while
 
-	_printChoice(selection_tuple[cursor])
+	_printChoice(selection_tuple[cursor],first=True)
 	while(True):
-		k=input()
-		print("a")
-		if k=='\x1b[A':
+		k=getConsoleArrowInput()
+		if k==KEY_RETURN: break
+		if k==KEY_UP:
 			if cursor==0: continue
 			cursor-=1
 		#end up
-		if k=='\x1b[B':
+		if k==KEY_DOWN:
 			if cursor==len(selection_tuple)-1: continue
 			cursor+=1
 		#end down
@@ -103,7 +141,7 @@ settings['speaking_rate']=inputWithCheck("音声速度の倍率(0.25～4.0)倍",
 settings['pitch']=inputWithCheck("音声の音程(-20.0～20.0)セミトーン",settings['pitch'],-20.0,20.0)
 settings['volume_gain_db']=inputWithCheck("音声の音量(-96.0～16.0)dB",settings['volume_gain_db'],-96.0,16.0)
 #音声プロファイルは作ってる途中。矢印で選択させたいけどできない。
-#settings['effects_profile_id']=showMenu("適用する音声プロファイル",PROFILES,settings['effects_profile_id'])
+settings['effects_profile_id']=showMenu("適用する音声プロファイル",PROFILES,settings['effects_profile_id'])
 print("設定を保存中...")
 with open('settings.json', 'w', encoding='UTF-8') as f:
 	f.write(json.dumps(settings))
